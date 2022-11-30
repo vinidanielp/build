20b40316f7a43324e2fb24c24c860bafc8880265
Vinicius Pretti <vinicius.pretti2@hotmail.com>
2022-11-30 10:14:38 -0300

    criando front-end do projeto

---
+++ front/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Resume from "./components/Resume";
+import Form from "./components/Form";
+import GlobalStyle from "./styles/global";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Resume />
+      <Form />
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
